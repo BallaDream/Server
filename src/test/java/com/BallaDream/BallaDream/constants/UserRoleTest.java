f7c53d1db1451df6355622ea44e0d337428f6d26
won-jae-jang <dnjswo410@naver.com>
2025-04-03 17:59:04 +0900

    test fix

--- src/test/java/com/BallaDream/BallaDream/constants/UserRoleTest.java
+++ src/test/java/com/BallaDream/BallaDream/constants/UserRoleTest.java
@@ -1,6 +1,5 @@
 package com.BallaDream.BallaDream.constants;
 
-import com.BallaDream.BallaDream.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +9,7 @@ class UserRoleTest {
     void userRoleEqualTest() {
 
         //given
-        User user = new User("test", UserRole.ROLE_USER);
         //when
-        UserRole roleUser = UserRole.valueOfRole("ROLE_USER");
         //then
-        Assertions.assertThat(roleUser).isNotNull();
     }
 }
