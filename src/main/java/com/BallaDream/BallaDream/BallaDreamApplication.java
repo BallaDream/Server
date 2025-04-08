7400c33272439426127457c983e416cd916c554b
won-jae-jang <dnjswo410@naver.com>
2025-04-08 18:01:54 +0900

    domain fix

--- src/main/java/com/BallaDream/BallaDream/BallaDreamApplication.java
+++ src/main/java/com/BallaDream/BallaDream/BallaDreamApplication.java
@@ -35,14 +35,14 @@ public class BallaDreamApplication {
 		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
 
 		return tomcat;
-	}
+	}*/
 
-	*//*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+
+    /*We need to redirect from HTTP to HTTPS. Without SSL, this application used
     port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     *//*
-	private Connector httpToHttpsRedirectConnector() {
+    redirected to HTTPS on 8443.*/
+
+	/*private Connector httpToHttpsRedirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
 		connector.setPort(8080);
