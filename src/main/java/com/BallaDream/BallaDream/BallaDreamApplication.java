8af3fedbb883d8caa1884a64ff934f252d6a4528
won-jae-jang <dnjswo410@naver.com>
2025-07-09 11:14:40 +0900

    ssl 변경

--- src/main/java/com/BallaDream/BallaDream/BallaDreamApplication.java
+++ src/main/java/com/BallaDream/BallaDream/BallaDreamApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BallaDreamApplication {
-
+	//ssl 설정 변경
 	public static void main(String[] args) {
 		SpringApplication.run(BallaDreamApplication.class, args);
 	}
